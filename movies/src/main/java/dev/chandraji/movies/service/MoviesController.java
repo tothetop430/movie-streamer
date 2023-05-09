0076f6f1cde4bec906259ed496dac442df377a7e
tothetop430 <jhuseyrer355545@gmail.com>
2023-05-09 11:59:19 +0530

    add GetMapping by imdbId

--- movies/src/main/java/dev/chandraji/movies/service/MoviesController.java
+++ movies/src/main/java/dev/chandraji/movies/service/MoviesController.java
@@ -1,16 +1,32 @@
 package dev.chandraji.movies.service;
 
+import dev.chandraji.movies.data.Movie;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MoviesController {
 
+    @Autowired
+    private MovieService movieService;
+
     @GetMapping
-    public String getAllMovies() {
-        return "All Movies!";
+    public ResponseEntity<List<Movie>> getAllMovies() {
+        return new ResponseEntity<>(movieService.allMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Movie>> findAMovie(@PathVariable String id) {
+        return new ResponseEntity<>(movieService.findAMovie(id), HttpStatus.OK);
     }
 
 }
