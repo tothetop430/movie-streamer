a89003831aaf91308cd2a8039c53bc98d14ccc65
tothetop430 <jhuseyrer355545@gmail.com>
2023-05-09 12:33:01 +0530

    create ReviewController

---
+++ movies/src/main/java/dev/chandraji/movies/service/ReviewController.java
@@ -0,0 +1,26 @@
+package dev.chandraji.movies.service;
+
+import dev.chandraji.movies.data.Review;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/movies")
+public class ReviewController {
+
+    @Autowired
+    private ReviewService reviewService;
+
+    @PostMapping("/review")
+    public ResponseEntity<Review> createReview(@RequestBody Map<String, String> payload) {
+        return new ResponseEntity<>(reviewService.createReview(payload.get("reviewBody"), payload.get("imdbId")),
+                HttpStatus.CREATED);
+    }
+}
