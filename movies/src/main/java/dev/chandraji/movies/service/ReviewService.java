233ee2653be3086a646f1a1c64ee054acfe02747
tothetop430 <jhuseyrer355545@gmail.com>
2023-05-09 12:34:28 +0530

    create ReviewService

---
+++ movies/src/main/java/dev/chandraji/movies/service/ReviewService.java
@@ -0,0 +1,31 @@
+package dev.chandraji.movies.service;
+
+import dev.chandraji.movies.data.Movie;
+import dev.chandraji.movies.data.Review;
+import dev.chandraji.movies.data.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public Review createReview(String reviewBody, String imdbId) {
+        Review review = new Review(reviewBody);
+        reviewRepository.insert(review);
+
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviewIds").value(review))
+                .first();
+        return review;
+    }
+}
